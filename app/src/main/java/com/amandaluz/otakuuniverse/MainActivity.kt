e10f402273de25a6491a010f1ced060124c622d3
feandrade <feandrade@brq.com>
2023-02-28 14:02:24 -0300

    criando navegação e menu superior com drawerLayout

--- app/src/main/java/com/amandaluz/otakuuniverse/MainActivity.kt
+++ app/src/main/java/com/amandaluz/otakuuniverse/MainActivity.kt
@@ -2,19 +2,31 @@ package com.amandaluz.otakuuniverse
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.amandaluz.otakuuniverse.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfig: AppBarConfiguration
     private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        toolbar()
+    }
+
+    private fun toolbar() {
         navigationController()
+        appBarConfig = AppBarConfiguration(navController.graph, binding.container)
+        binding.toolbar.setupWithNavController(navController, appBarConfig)
+        binding.navView.setupWithNavController(navController)
     }
 
     private fun navigationController() {
